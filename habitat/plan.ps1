e62b03eca7d57972cfd3ebf9ee6c2e1c34509fb4
Alain <alubin@chef.io>
2019-10-07 00:46:04 +0000

    update CSS background to Blue, updated version on plan.ps1

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -1,6 +1,6 @@
 $pkg_name="nop-commerce"
 $pkg_origin="alain"
-$pkg_version="0.1.5"
+$pkg_version="0.1.6"
 $pkg_maintainer="Alain Lubin alubin@chef.io"
 $pkg_license=@("MIT")
 $pkg_upsteam_url="https://github.com/devopslifter/nopCommerce"
